bac440104511e8780b3679055b69e8f5f7b91a42
kota makino <kota.m.2774@gmail.com>
2021-05-08 12:37:40 +0900

    1. 完了

--- search.py
+++ search.py
@@ -1,9 +1,11 @@
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 def search():
-    word = input("鬼滅の登場人物の名前を入力してください >>> ")
-
-    print("{}が見つかりました".format(word))
+    input_name = input("鬼滅の登場人物の名前を入力してください >>> ")
+    if input_name in source:
+        print("{}が見つかりました".format(input_name))
+    else:
+        print("{}は見つかりませんでした".format(input_name))
 
 if __name__ == "__main__":
     search()
