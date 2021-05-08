0156ff6016c3f83c366e633c8279c44159847e87
kota makino <kota.m.2774@gmail.com>
2021-05-08 13:09:58 +0900

    3. 完了

--- search.py
+++ search.py
@@ -1,8 +1,13 @@
 import csv
 
+with open('./source.csv', mode="r", encoding="utf-8") as f:
+    source = csv.reader(f, delimiter="\n")
+    source_list = sum(list(source), [])
+
+
 def search():
     input_name = input("鬼滅の登場人物の名前を入力してください >>> ")
-    if input_name in source:
+    if input_name in source_list:
         print("{}が見つかりました".format(input_name))
     else:
         print("{}は見つかりませんでした".format(input_name))
